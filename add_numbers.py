30fd4dd203e95eb5556765c26354486cfdcd9ef8
AwaisKhalil-47 <mkhalil.bscs25seecs@seecs.edu.pk>
2025-12-02 11:22:26 +0500

    Add three python function files

---
+++ add_numbers.py
@@ -0,0 +1,8 @@
+def add_numbers(num1, num2):
+  return num1 + num2
+
+
+result = add_numbers(5, 3)
+print(f"The sum of 5 and 3 is: {result}")
+result_float = add_numbers(10.5, 2.3)
+print(f"The sum of 10.5 and 2.3 is: {result_float}")
